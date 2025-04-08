5c4dd3b6537e1380273e2c41961b570bd75ed82e
Kenneth Lee <n11532386@qut.edu.au>
2025-04-08 22:12:58 +1000

    Attention function complete

--- FSharpTransformer/Attention.fs
+++ FSharpTransformer/Attention.fs
@@ -28,30 +28,36 @@ let weightedAttention (attention: Vector) (valueLookup:int->float) : float =
 // This is equivalent to the n_heads loop in the transformer() function in the C# implementation.    
 let attentionForOneHead (keyLookup:int->int->float) (valueLookup:int->int->float) (tokenPosition:int) (query: Vector): Vector =
     // TODO: Implement this function.
-    // Determine the size of the query vector (also the size of the key and value vectors)
-    let headSize = query.Length
-    // Array to hold the attention scores for each token (from 0 to tokenPosition)
-    let attentionScores = Array.zeroCreate<float> (tokenPosition + 1)
-    let scores = [| for i in 0 .. tokenPosition -> attentionScore query (fun j -> keyLookup i j ) |]
+    //// Compute attention scores for each token
+    let scores = 
+        [|0 .. tokenPosition|]
+        |> Array.map (fun i -> attentionScore query (fun j -> keyLookup i j))
+    //Compute softmax weights
     let weights = softMax scores
-    let outputVector = [| for j in 0 .. (Array.length query) - 1 -> Array.sum [| for i in 0 .. tokenPosition -> (valueLookup i j) * weights.[i] |] |]
-    //let outputVector = 
-    //    query
-    //        |> Array.mapi (fun j _ ->
-    //            [| 0 .. tokenPosition |]
-    //        |> Array.mapi (fun i _ -> valueLookup i j * weights.[i])
-    //        |> Array.sum
-    //)
+
+    // Compute the output vector using weighted sum of value vectors
+    let outputVector = 
+        Array.init (Array.length query) (fun j ->
+            [|0 .. tokenPosition|]
+            |> Array.map (fun i -> (valueLookup i j) * weights.[i])
+            |> Array.sum)
     outputVector
     //raise (System.NotImplementedException("Attention attentionForOneHead not implemented")) 
-
-    //raise (System.NotImplementedException("Attention attentionForOneHead not implemented"))
 
 // Computes attention for all heads in multi-head attention.
 // Hint: Instead of returning multiple vectors, one for each head, this array should be flattened with flattenMultipleHeads().
 let attention (keyLookup:int->int->int->float) (valueLookup:int->int->int->float) (tokenPosition:int) (query: MultiHead) : Vector =
     // TODO: Implement this function
-    raise (System.NotImplementedException("Attention attention not implemented"))
-    //[| for i in 0 .. tokenPosition -> attentionForOneHead (fun j -> keyLookup i j) (fun j -> valueLookup i j) i qu
-    //ery |]
-    //    |> Array.concat
+    // Compute the attention for each head
+    [| 0 .. (Array.length query) - 1 |]  // Iterate over the number of heads
+    |> Array.map (fun headIndex -> 
+        // For each head, compute the attention for all tokens using query[headIndex]
+        attentionForOneHead 
+            (fun i -> keyLookup headIndex i)    // Key lookup for this head
+            (fun i -> valueLookup headIndex i)  // Value lookup for this head
+            tokenPosition 
+            query.[headIndex]                   // The query vector for this head
+    )
+    |> flattenMultipleHeads
+
+    //raise (System.NotImplementedException("Attention attention not implemented"))
